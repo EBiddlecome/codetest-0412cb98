d888454d4b14c374da5b30dd603759643c1ebf66
EBiddlecome <elizabethwbiddlecome@gmail.com>
2016-09-30 22:44:22 -0700

    configured server and intital routing, wrote & passed first api test (inline, will probably separate later)

--- app/server.js
+++ app/server.js
@@ -1,2 +1,27 @@
 //quick server test
-console.log('“If you spend too much time thinking about a thing, you’ll never get it done.” ~ Bruce Lee');
+// console.log('“If you spend too much time thinking about a thing, you’ll never get it done.” ~ Bruce Lee');
+
+var express = require('express');
+var app = express();   
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router(); 
+
+// test route to see if working
+// accessed at GET http://localhost:8080/tasks
+
+router.get('/', function(req, res) {
+  res.json(
+		{ message: 'Router functional' }
+	);   
+});
+
+app.use('/tasks', router);
+app.listen(port);
+
+console.log('SuDoIt Server running on port ' + port);
